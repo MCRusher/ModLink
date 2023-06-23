a77681af13f70a5a788403eb918647ceed8ea16b
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2023-06-23 11:32:05 -0400

    Add implementation

--- src/ModLink.nim
+++ src/ModLink.nim
@@ -1,5 +1,16 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import os, strformat
 
-when isMainModule:
-  echo("Hello, World!")
+let args = commandLineParams()
+
+if args.len != 3:
+    quit(&"Usage: ./{getAppFilename()} [-r|-a] <mod groups src dir> <mod dest dir>\nOptions:\n -a : add mode\n -r : remove mode")
+
+let remove = args[0] == "-r"
+let src = args[1]
+let dest = args[2]
+
+for file in src.walkDirRec():
+    if not remove:
+        createHardlink(file, dest / file.extractFilename())
+    else:
+        removeFile(dest / file.extractFilename())
